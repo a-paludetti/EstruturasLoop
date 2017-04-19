1d7fe5a40fdcaf4bcc59dfda33613dc51db9b5c7
Wynter <Wynter@192.168.100.115>
2017-04-18 22:54:41 -0300

    exercicio 7 (incompleto)

---
+++ EstruturasLoop/src/estruturasloop/Es_07.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estruturasloop;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Wynter
+ */
+public class Es_07 {
+    public static void main(String[] args) {
+        Scanner NN = new Scanner(System.in);
+        
+        System.out.print("Escreva algo: ");
+        String palavra = NN.nextLine();
+        
+        palavra = palavra.toUpperCase();
+        
+    }
+}
